438bdc149805612dd0efb1625e4329ad2bcc81d5
Pinball Wiz <70185819+pinballwizz@users.noreply.github.com>
2025-05-25 07:50:25 +1000

    Update make_mooncrgx_proms.bat

--- DE2-MoonCresta/tools/make_mooncrgx_proms.bat
+++ DE2-MoonCresta/tools/make_mooncrgx_proms.bat
@@ -4,7 +4,6 @@ copy /B 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 main.bin
 
 
 make_vhdl_prom main.bin rom0.vhd
-
 make_vhdl_prom 1h.bin galaxian_1h.vhd
 make_vhdl_prom 1k.bin galaxian_1k.vhd
 make_vhdl_prom l06_prom.bin galaxian_6l.vhd
